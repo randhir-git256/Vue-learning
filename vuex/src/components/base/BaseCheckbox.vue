a071a66ffd28bdb465d6ef2d78589b6b95c157db
randhir-git256 <120366000+randhir-git256@users.noreply.github.com>
2023-09-06 00:07:53 +0530

    Add files via upload

---
+++ vuex/src/components/base/BaseCheckbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex items-center">
+    <input
+      type="checkbox"
+      class="flex-none h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2"
+      :checked="modelValue"
+      @change="onChange"
+    />
+    <label>
+      <slot>Checkbox</slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    onChange() {
+      this.$emit("update:modelValue", !this.modelValue);
+    },
+  },
+};
+</script>
