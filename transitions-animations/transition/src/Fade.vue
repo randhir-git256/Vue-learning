62876a682844578d4ba16127945f4747c67fa799
randhir-git256 <120366000+randhir-git256@users.noreply.github.com>
2023-09-06 10:21:44 +0530

    Add files via upload

---
+++ transitions-animations/transition/src/Fade.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const show = ref(true)
+</script>
+
+<template>
+  <button @click="show = !show">Toggle</button>
+  <Transition name="fade">
+    <p v-if="show">Fade animation</p>
+  </Transition>
+</template>
